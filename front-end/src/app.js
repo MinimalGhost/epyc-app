0671ad79daef7df7cf9fc4200eef8b7c376922e5
yamunanavada <yamunanavada@gmail.com>
2018-01-22 14:47:22 -0500

    made some front end edits

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,4 +1,28 @@
 class App {
   static init() {
   }
+
+  // handle createNewGame
+
+
+  // handle startGame
+
+
+  // handle turns
+
+
+
+  // handle sumbitInput
+
+
+  
+
+
+
+
+
+
+
+
+
 }
--- front-end/src/game.js
+++ front-end/src/game.js
@@ -4,5 +4,30 @@ class Game {
     this.title = title
     this.status = status
     this.num_of_players = num_of_players
+    this.turns = num_of_players
   }
+
+  getUsers(){
+    return userStore.filter(user => user.game_id === this.id)
+  }
+
+  getGameCards(){
+
+  }
+
+  changeStatus(){
+    if(num_of_players === this.getUsers().length){
+      this.status = "in progess"
+            // "Pending" changes to "In progress" when num_of_players matches total number of users with the associated game id
+          // FETCH - PATCH: change status from "pending" to "in progress" to "complete"
+    } else if (this.turns === 0){
+        this.status = "complete"
+      // "in progress" changes to "complete" when number of turns = 0
+    }
+  }
+
+
+
+
+
 }
