014ca0c8152a1c66fb88cc24386fc438c50ae3d8
yamunanavada <yamunanavada@gmail.com>
2018-01-24 18:17:57 -0500

    latest

--- epyk-backend-app/app/models/game.rb
+++ epyk-backend-app/app/models/game.rb
@@ -1,4 +1,14 @@
 class Game < ApplicationRecord
   has_many :users
   has_many :cards, through: :users
+  # after_save :update_turn
+  #
+  #
+  # def update_turn
+  #   # if number of entreis === turns * users
+  #   game.turns += 1
+  #   game.save
+  # end
+  #
+  #
 end
